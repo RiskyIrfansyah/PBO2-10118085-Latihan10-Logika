f0c05cc3a9c138cae171f915d1776b57ea4c61ff
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-09-26 08:08:08 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118085/latihan10/PBO210118085Latihan10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan10;
+
+/**
+ * @author 
+ * NAMA                 : RISKY IRFRANSYAH
+ * KELAS                : IF-2
+ * NIM                  : 10118085
+ * DESKRIPSI PROGRAM    : Logika
+ * @author RISKY
+ */
+public class PBO210118085Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
